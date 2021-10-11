e1976742905591cf214255a686063ecd7769a1a8
Stefano De Capitani <stefano.deca@live.com>
2021-10-11 15:37:26 +0200

    hamburger menu opens and closes on click

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const hamburgerBtn = document.querySelector(".header-right > a");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const closeBtn = document.querySelector(".close");
+
+hamburgerBtn.addEventListener("click", function () {
+  hamburgerMenu.classList.add("active");
+  closeBtn.addEventListener("click", function () {
+    hamburgerMenu.classList.remove("active");
+  });
+});
